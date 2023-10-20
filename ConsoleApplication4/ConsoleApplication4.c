c9ec9406be305a34f29e27d5f28e432186aa915c
ArturCodeAcademy <artur.radzivilov@codeacademylt.onmicrosoft.com>
2023-10-20 09:37:58 +0300

    Refactor

--- ConsoleApplication4/ConsoleApplication4.c
+++ ConsoleApplication4/ConsoleApplication4.c
@@ -20,19 +20,21 @@ int NullIfOdd(int value)
 
 void ListTest()
 {
-	int arr[] = { 1, 2, 3, 4, 5 };
-	struct LinkedList list = CreateLinkedListFromArray(arr, 5);
+	int arr[] = { 1, 2, 3, 4, 5, 1, 20 };
+	struct LinkedList list = CreateLinkedListFromArray(arr, 7);
 
 
 	PrintList(&list);
-	// RemoveElement(&list, 5);
-	// RemoveNode(&list, n);
-	// PrintList(&list);
-	struct node* f = FindNode(&list, 5);
-	struct node* l = FindLastNode(&list, 5);
+	RemoveElement(&list, 5);
+	PrintList(&list);
+	struct node* f = FindNode(&list, 1);
+	struct node* l = FindLastNode(&list, 1);
 
 	printf("Found node: %d, Next: %d\n", f->data, f->next->data);
 	printf("Found last node: %d, Next: %d\n", l->data, l->next->data);
+
+	RemoveNode(&list, f);
+    PrintList(&list);
 }
 
 void TestDArray()
@@ -74,7 +76,10 @@ void TestDArray()
 
 int main()
 {
-	//ListTest();
+    printf("List test\n");
+	ListTest();
+
+    printf("\nDinamic array test\n");
 	TestDArray();
 
 	return 0;
